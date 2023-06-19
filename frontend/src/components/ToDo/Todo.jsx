8fd4465558bac525d205ace01dd18a7d10ecdd11
Raunak Singh Kalsi <98894054+code-wizard123@users.noreply.github.com>
2023-06-19 15:06:17 +0530

    Landing page design + To Do list frontend

---
+++ frontend/src/components/ToDo/Todo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './css/Todo.css'
+
+const Todo = () => {
+  return (
+    <div>
+      <div className='buttons'>
+        <div className="button">Mark Task</div>
+        <div className="button">Add Task</div>
+      </div>
+      <div className='list'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Todo
