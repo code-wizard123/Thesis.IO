459d96258c7ba654b17021fd16cfc2cb4ee21322
SID <bahetisid06@gmail.com>
2023-06-24 21:32:13 +0530

    Duplicated and routed differently

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import Chat from './components/Chat';
 import Landing from './components/Landing'
 import Home from './components/Home'
-import Login from './components/Login'
+
+import StudentLogin from './components/StudentLogin'
+import StudentSignup from './components/StudentSignup';
+
+import MentorLogin from './components/MentorLogin';
+import MentorSignup from './components/MentorSignup';
+
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
 
@@ -12,7 +18,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Chat" element={<Chat />} />
         <Route path="/landing" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
+
+        <Route path="/student/login" element={<StudentLogin />} />
+        <Route path="/student/signup" element={<StudentSignup />} />
+
+        <Route path="/mentor/login" element={<MentorLogin />} />
+        <Route path="/mentor/signup" element={<MentorSignup />} />
+
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/MentorLogin.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import LoginCSS from '../css/Login.module.css';
+
+const MentorLogin = () => {
+    const [uemail, setEmail] = useState('');
+    const [upassword, setPassword] = useState('');
+    const [error , seterror] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        const response = await fetch("/api/student/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email: uemail, password: upassword}),
+        })
+
+        const json = await response.json();
+
+        if (json.success) {
+            console.log("Login Success")
+        }
+
+        if (json.error) {
+            seterror(json.error)
+            setTimeout(() => {
+                seterror(null)
+            }, 4000)
+        }
+
+    }
+
+    return (
+        <div className={LoginCSS.outer}>
+            <form className={LoginCSS.form} autoComplete='off' autoCorrect='on' onSubmit={(e) => handleSubmit(e)}>
+                <h1 className={LoginCSS.formheader}>Login</h1>
+                <label className={LoginCSS.formlabel} htmlFor='username'>Email</label>
+                <input id="username" className={LoginCSS.input} type="email" onChange={(e) => setEmail(e.target.value)}></input>
+                <label className={LoginCSS.formlabel} htmlFor='password'>Password</label>
+                <input id="password" className={LoginCSS.input} type="password" onChange={(e) => setPassword(e.target.value)}></input>
+                <button type='submit'>Submit</button>
+                {error && <div> {error} </div>}
+            </form>
+        </div>
+    )
+}
+
+export default MentorLogin
